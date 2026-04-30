20ec5c6f4d0e3c9d0a9c53f067bc72cb70938723
Fron4i <ilya10306@mail.ru>
2026-04-30 15:19:51 +0300

    infra/configure-sidekiq-with-redis

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+require "sidekiq/web"
+
+Sidekiq::Web.use Rack::Auth::Basic do |user, password|
+  ActiveSupport::SecurityUtils.secure_compare(user, ENV.fetch("SIDEKIQ_USER")) &
+    ActiveSupport::SecurityUtils.secure_compare(password, ENV.fetch("SIDEKIQ_PASSWORD"))
+end
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/admin/sidekiq"
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
